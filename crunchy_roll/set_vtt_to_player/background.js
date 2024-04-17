544d7de3c1a2b593364f76ba3299a43e729baa50
hahacandy <58638224+hahacandy@users.noreply.github.com>
2024-04-17 15:07:11 +0900

    Update background.js

--- crunchy_roll/set_vtt_to_player/background.js
+++ crunchy_roll/set_vtt_to_player/background.js
@@ -1,39 +1,17 @@
-async function asyncExecuteScript(tabId, details) {
-    var result = await getExecuteScriptPromise(tabId, details)
-        .then((result) => {return result;})
-        .catch((ex) => {console.log("asyncExecuteScript = " + ex.message);});
-    return result;   
-}
-const getExecuteScriptPromise = (...args) => {
-    return new Promise((resolve, reject) => {
-        chrome.tabs.executeScript(...args, () => {
-            let lastError = chrome.runtime.lastError;
-            if (lastError) return reject(lastError);
-            resolve(true);
-        });
-    });
-}
-
 chrome.runtime.onInstalled.addListener(function() {
   chrome.webRequest.onBeforeRequest.addListener(
     async function(details) {
 		
-		vttPos = details.url.indexOf(".vtt");
+		vttPos = details.url.indexOf("sub-f1.vtt");
 		if (vttPos > 0) {
-			urlAttribPos = details.url.indexOf("?");
-			if (vttPos < urlAttribPos || urlAttribPos < 0) {
-				let tabId = details.tabId;
-				if (tabId < 0) return;
-				let requestVtt = details.url;
-				
-				
-				
-				chrome.storage.sync.set({'vtt_url': requestVtt}, function() {
-					console.log(requestVtt);
-					console.log('Settings saved');
-				});
-
-				
+			let tabId = details.tabId;
+			if (tabId < 0) return;
+			let requestVtt = details.url;
+			
+			chrome.storage.sync.set({'vtt_url': requestVtt}, function() {
+				console.log(requestVtt);
+				console.log('Settings saved');
+			});
 			}
 		}
 
