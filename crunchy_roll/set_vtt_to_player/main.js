f250151db259bd8884da6380b8ee1edec9b906d4
hahacandy <58638224+hahacandy@users.noreply.github.com>
2024-04-27 01:39:27 +0900

    Update main.js

--- crunchy_roll/set_vtt_to_player/main.js
+++ crunchy_roll/set_vtt_to_player/main.js
@@ -1,6 +1,6 @@
 setInterval(function() {
 	try{
+		document.querySelector('#content > div > div > div.app-body-wrapper > div > div > div.video-player-wrapper > div').style.maxHeight = 'calc(120vh - 15.625rem)';
 		document.getElementsByClassName('video-player')[0].focus();
-		document.querySelector('#content > div > div.app-body-wrapper > div > div > div.video-player-wrapper > div').style.maxHeight = 'calc(120vh - 15.625rem)';
 	}catch{}
 }, 100);
