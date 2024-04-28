4cfaebf65f8833a42ea64a0b289002c4563f5f7b
hahacandy <58638224+hahacandy@users.noreply.github.com>
2024-04-29 00:39:39 +0900

    Update background.js

--- crunchy_roll/set_vtt_to_player/background.js
+++ crunchy_roll/set_vtt_to_player/background.js
@@ -1,14 +1,12 @@
 console.log('background test')
-chrome.runtime.onInstalled.addListener(function() {
-  chrome.webRequest.onBeforeRequest.addListener(
-    async function(details) {
-		
+chrome.webRequest.onBeforeRequest.addListener(
+	async function(details) {
 		vttPos = details.url.indexOf(".vtt");
 		if (vttPos > 0) {
 			urlAttribPos = details.url.indexOf("?");
 			if (vttPos < urlAttribPos || urlAttribPos < 0) {
-				let tabId = details.tabId;
-				if (tabId < 0) return;
+				//let tabId = details.tabId;
+				//if (tabId < 0) return;
 				let requestVtt = details.url;
 				
 				chrome.storage.local.set({'vtt_url': requestVtt}, function() {
@@ -19,7 +17,7 @@ chrome.runtime.onInstalled.addListener(function() {
 			}
 		}
 
-    },
-    { urls: ["<all_urls>"] }
-  );
-});
+	},
+	{ urls: ["<all_urls>"] }
+);
+
