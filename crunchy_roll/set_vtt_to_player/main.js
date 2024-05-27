b56d3f749e8da8718cc4da78db750dec3165c4f2
hahacandy <58638224+hahacandy@users.noreply.github.com>
2024-05-28 07:57:10 +0900

    마우스가 스크롤바 위쪽에 있으면 헤더 생기고 내리면 헤더사라짐

--- crunchy_roll/set_vtt_to_player/main.js
+++ crunchy_roll/set_vtt_to_player/main.js
@@ -1,6 +1,28 @@
+var is_hide_header = false;
+var mouse_event_time = new Date();
 setInterval(function() {
 	try{
-		document.querySelector('#content > div > div > div.app-body-wrapper > div > div > div.video-player-wrapper > div').style.maxHeight = window.innerHeight-60+'px';
+		if(new Date().getTime() - mouse_event_time.getTime() >= 5000){ //5초이상 가만히있을시 헤더 사라짐
+			is_hide_header = true;
+			document.querySelector('#content > div > div > div.erc-large-header').style.display='none';
+		}
+		
+		if(is_hide_header == false)
+			document.querySelector('#content > div > div > div.app-body-wrapper > div > div > div.video-player-wrapper > div').style.maxHeight = window.innerHeight-60+'px';
+		else
+			document.querySelector('#content > div > div > div.app-body-wrapper > div > div > div.video-player-wrapper > div').style.maxHeight = window.innerHeight+'px';
 		document.getElementsByClassName('video-player')[0].focus();
 	}catch{}
 }, 100);
+
+window.addEventListener('mousemove', (event) => {
+	if(event.clientY > 100){
+		document.querySelector('#content > div > div > div.erc-large-header').style.display='none';
+		is_hide_header = true;
+	}else{
+		document.querySelector('#content > div > div > div.erc-large-header').style.display=''
+		is_hide_header = false;
+	}
+	
+	mouse_event_time = new Date(); 
+});
