fd6868082cea8eda39d6b33d0edb498da4e929df
hahacandy <58638224+hahacandy@users.noreply.github.com>
2024-12-23 09:50:42 +0900

    Update content.js

--- youtube/my-yt-ad-skipper/content.js
+++ youtube/my-yt-ad-skipper/content.js
@@ -4,20 +4,20 @@
 const checkAndRemoveAds = () => {
   try {
     // 1. 광고 배지(요소) 클래스
-    const adBadge = document.querySelector(
-      '.ad-simple-attributed-string.ytp-ad-badge__text--clean-player.ytp-ad-badge__text--clean-player-with-light-shadow'
-    );
+    //const adBadge = document.querySelector(
+    //  '.ad-simple-attributed-string.ytp-ad-badge__text--clean-player.ytp-ad-badge__text--clean-player-with-light-shadow'
+    //);
 
     // 광고 배지가 있으면 광고로 간주
-    if (adBadge) {
-      const video = document.querySelector('video');
-      // 영상 객체가 있고, 재생 길이가 0보다 클 때만 실행
-      if (video && video.duration > 0) {
-        // 광고 구간 끝으로 강제 이동
-        video.currentTime = video.duration-3;
-        console.log('광고 구간을 건너뛰었습니다!');
-      }
-    }
+    //if (adBadge) {
+    //  const video = document.querySelector('video');
+    //  // 영상 객체가 있고, 재생 길이가 0보다 클 때만 실행
+    //  if (video && video.duration > 0) {
+    //    // 광고 구간 끝으로 강제 이동
+    //    video.currentTime = video.duration-3;
+    //    console.log('광고 구간을 건너뛰었습니다!');
+    //  }
+    //}
 
     // 2. <div id="player-ads" class="style-scope ytd-watch-flexy"> 요소 삭제
     const playerAds = document.querySelector('#player-ads.style-scope.ytd-watch-flexy');
